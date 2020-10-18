7660642f5df2b391dd710772bd46dcf18415380d
Antoine Huret <ahuret@skilld.cloud>
2020-10-19 01:22:21 +0200

    added having spec / limit spec / virtual columns

--- query/virtualcolumn/virtual_column.go
+++ query/virtualcolumn/virtual_column.go
@@ -1,1 +1,36 @@
 package virtualcolumn
+
+import (
+	"encoding/json"
+
+	"github.com/grafadruid/go-druid/query"
+)
+
+type Base struct {
+	Type string `json:"type"`
+}
+
+func NewBase() *Base {
+	b := &Base{}
+	return b
+}
+
+func (b *Base) SetType(typ string) *Base {
+	b.Type = typ
+	return b
+}
+
+func Load(data []byte) (query.Dimension, error) {
+	var t struct {
+		Typ string `json:"type"`
+	}
+	if err := json.Unmarshal(data, &t); err != nil {
+		return nil, err
+	}
+	var d query.Dimension
+	switch t.Typ {
+	case "expression":
+		d = NewExpression()
+	}
+	return d, json.Unmarshal(data, &d)
+}
