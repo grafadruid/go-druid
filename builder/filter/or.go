19b81238d0f8d9a7496f4ea53cb047f1049b0d8f
Vigith Maurice <vigith@gmail.com>
2021-01-19 15:13:14 -0800

    SetField should accept a slice

--- builder/filter/or.go
+++ builder/filter/or.go
@@ -1,8 +1,13 @@
 package filter
 
+import (
+	"encoding/json"
+	"github.com/grafadruid/go-druid/builder"
+)
+
 type Or struct {
 	Base
-	Fields []string `json:"fields,omitempty"`
+	Fields []builder.Filter `json:"fields,omitempty"`
 }
 
 func NewOr() *Or {
@@ -11,7 +16,29 @@ func NewOr() *Or {
 	return o
 }
 
-func (o *Or) SetFields(fields []string) *Or {
+func (o *Or) SetFields(fields []builder.Filter) *Or {
 	o.Fields = fields
 	return o
+}
+
+func (o *Or) UnmarshalJSON(data []byte) error {
+	var err error
+	var tmp struct {
+		Base
+		Fields []json.RawMessage `json:"fields,omitempty"`
+	}
+	if err = json.Unmarshal(data, &tmp); err != nil {
+		return err
+	}
+	var f builder.Filter
+	ff := make([]builder.Filter, len(tmp.Fields))
+	for i := range tmp.Fields {
+		if f, err = Load(tmp.Fields[i]); err != nil {
+			return err
+		}
+		ff[i] = f
+	}
+	o.Base = tmp.Base
+	o.Fields = ff
+	return nil
 }
