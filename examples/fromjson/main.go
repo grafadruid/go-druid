1ae6aebc36d549c55a26922d6ef25a4ca567be6d
Vigith Maurice <vigith@gmail.com>
2021-01-24 09:03:40 -0800

    refactor main to exit on error (3 queries are failing)

--- examples/fromjson/main.go
+++ examples/fromjson/main.go
@@ -9,6 +9,22 @@ import (
 	"github.com/grafadruid/go-druid/builder"
 )
 
+func loadAndExecute(d *druid.Client, qry []byte) {
+	var err error
+	var q builder.Query
+	var results interface{}
+	q, err = d.Query().Load(qry)
+	if err != nil {
+		log.Fatalf("Load failed, %s (query: %s)", err, string(qry))
+	}
+	spew.Dump(q)
+	_, err = d.Query().Execute(q, &results)
+	if err != nil {
+		log.Fatalf("Load failed, %s (query: %s)", err, string(qry))
+	}
+	spew.Dump(results)
+}
+
 func main() {
 	d, err := druid.NewClient("http://localhost:8082")
 	if err != nil {
@@ -20,71 +36,44 @@ func main() {
 	}
 	fmt.Println("{\"version\": \"" + status.Version + "\"}")
 
-	var q builder.Query
-	var results interface{}
+	var qry string
 
-	q, err = d.Query().Load([]byte("{\"queryType\":\"scan\",\"dataSource\":{\"type\":\"table\",\"name\":\"wikipedia\"},\"intervals\":{\"type\":\"intervals\",\"intervals\":[\"1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z\"]},\"virtualColumns\":[{\"type\":\"expression\",\"name\":\"v0\",\"expression\":\"'France'\",\"outputType\":\"STRING\"}],\"resultFormat\":\"compactedList\",\"batchSize\":20480,\"limit\":1,\"order\":\"none\",\"filter\":{\"type\":\"selector\",\"dimension\":\"countryName\",\"value\":\"France\",\"extractionFn\":null},\"columns\":[\"__time\",\"channel\",\"cityName\",\"comment\",\"count\",\"countryIsoCode\",\"diffUrl\",\"flags\",\"isAnonymous\",\"isMinor\",\"isNew\",\"isRobot\",\"isUnpatrolled\",\"metroCode\",\"namespace\",\"page\",\"regionIsoCode\",\"regionName\",\"sum_added\",\"sum_commentLength\",\"sum_deleted\",\"sum_delta\",\"sum_deltaBucket\",\"user\",\"v0\"],\"legacy\":false,\"context\":{\"sqlOuterLimit\":100,\"sqlQueryId\":\"b12ac7bb-7cc5-4873-b19d-1cd95264e01b\"},\"descending\":false,\"granularity\":{\"type\":\"all\"}}"))
-	spew.Dump(q)
-	d.Query().Execute(q, &results)
-	spew.Dump(results)
+	qry = `{"queryType":"scan","dataSource":{"type":"table","name":"wikipedia"},"intervals":{"type":"intervals","intervals":["1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z"]},"virtualColumns":[{"type":"expression","name":"v0","expression":"'France'","outputType":"STRING"}],"resultFormat":"compactedList","batchSize":20480,"limit":1,"order":"none","filter":{"type":"selector","dimension":"countryName","value":"France","extractionFn":null},"columns":["__time","channel","cityName","comment","count","countryIsoCode","diffUrl","flags","isAnonymous","isMinor","isNew","isRobot","isUnpatrolled","metroCode","namespace","page","regionIsoCode","regionName","sum_added","sum_commentLength","sum_deleted","sum_delta","sum_deltaBucket","user","v0"],"legacy":false,"context":{"sqlOuterLimit":100,"sqlQueryId":"b12ac7bb-7cc5-4873-b19d-1cd95264e01b"},"descending":false,"granularity":{"type":"all"}}`
+	loadAndExecute(d, []byte(qry))
 
-	q, err = d.Query().Load([]byte("{\"batchSize\":20480,\"columns\":[\"__time\",\"channel\",\"cityName\",\"comment\",\"count\",\"countryIsoCode\",\"diffUrl\",\"flags\",\"isAnonymous\",\"isMinor\",\"isNew\",\"isRobot\",\"isUnpatrolled\",\"metroCode\",\"namespace\",\"page\",\"regionIsoCode\",\"regionName\",\"sum_added\",\"sum_commentLength\",\"sum_deleted\",\"sum_delta\",\"sum_deltaBucket\",\"user\",\"v0\"],\"context\":{\"plopa\":\"plep\"},\"dataSource\":{\"name\":\"wikipedia\",\"type\":\"table\"},\"filter\":{\"dimension\":\"countryName\",\"type\":\"selector\",\"value\":\"France\"},\"intervals\":{\"type\":\"intervals\",\"intervals\":[\"1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z\"]},\"limit\":10,\"order\":\"none\",\"queryType\":\"scan\",\"virtualColumns\":[{\"expression\":\"'France'\",\"name\":\"v0\",\"outputType\":\"STRING\",\"type\":\"expression\"}]}"))
-	spew.Dump(q)
-	d.Query().Execute(q, &results)
-	spew.Dump(results)
+	qry = `{"batchSize":20480,"columns":["__time","channel","cityName","comment","count","countryIsoCode","diffUrl","flags","isAnonymous","isMinor","isNew","isRobot","isUnpatrolled","metroCode","namespace","page","regionIsoCode","regionName","sum_added","sum_commentLength","sum_deleted","sum_delta","sum_deltaBucket","user","v0"],"context":{"plopa":"plep"},"dataSource":{"name":"wikipedia","type":"table"},"filter":{"dimension":"countryName","type":"selector","value":"France"},"intervals":{"type":"intervals","intervals":["1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z"]},"limit":10,"order":"none","queryType":"scan","virtualColumns":[{"expression":"'France'","name":"v0","outputType":"STRING","type":"expression"}]}`
+	loadAndExecute(d, []byte(qry))
 
-	q, err = d.Query().Load([]byte("{\"batchSize\":20480,\"columns\":[\"__time\",\"channel\",\"cityName\",\"comment\",\"count\",\"countryIsoCode\",\"diffUrl\",\"flags\",\"isAnonymous\",\"isMinor\",\"isNew\",\"isRobot\",\"isUnpatrolled\",\"metroCode\",\"namespace\",\"page\",\"regionIsoCode\",\"regionName\",\"sum_added\",\"sum_commentLength\",\"sum_deleted\",\"sum_delta\",\"sum_deltaBucket\",\"user\"],\"dataSource\":{\"name\":\"wikipedia\",\"type\":\"table\"},\"filter\":{\"dimension\":\"countryName\",\"extractionFn\":{\"locale\":\"\",\"type\":\"lower\"},\"type\":\"selector\",\"value\":\"france\"},\"intervals\":{\"type\":\"intervals\",\"intervals\":[\"1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z\"]},\"limit\":10,\"order\":\"descending\",\"queryType\":\"scan\", \"resultFormat\":\"compactedList\"}"))
-	spew.Dump(q)
-	d.Query().Execute(q, &results)
-	spew.Dump(results)
+	qry = `{"batchSize":20480,"columns":["__time","channel","cityName","comment","count","countryIsoCode","diffUrl","flags","isAnonymous","isMinor","isNew","isRobot","isUnpatrolled","metroCode","namespace","page","regionIsoCode","regionName","sum_added","sum_commentLength","sum_deleted","sum_delta","sum_deltaBucket","user"],"dataSource":{"name":"wikipedia","type":"table"},"filter":{"dimension":"countryName","extractionFn":{"locale":"","type":"lower"},"type":"selector","value":"france"},"intervals":{"type":"intervals","intervals":["1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z"]},"limit":10,"order":"descending","queryType":"scan", "resultFormat":"compactedList"}`
+	loadAndExecute(d, []byte(qry))
 
-	q, err = d.Query().Load([]byte("{\"batchSize\":20480,\"columns\":[\"__time\",\"channel\",\"cityName\",\"comment\",\"count\",\"countryIsoCode\",\"diffUrl\",\"flags\",\"isAnonymous\",\"isMinor\",\"isNew\",\"isRobot\",\"isUnpatrolled\",\"metroCode\",\"namespace\",\"page\",\"regionIsoCode\",\"regionName\",\"sum_added\",\"sum_commentLength\",\"sum_deleted\",\"sum_delta\",\"sum_deltaBucket\",\"user\"],\"dataSource\":{\"type\":\"query\",\"query\":{\"queryType\":\"scan\",\"dataSource\":{\"type\":\"table\",\"name\":\"A\"},\"columns\":[\"AT\"],\"intervals\":{\"type\":\"intervals\",\"intervals\":[\"1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z\"]}}},\"filter\":{\"dimension\":\"countryName\",\"extractionFn\":{\"locale\":\"\",\"type\":\"lower\"},\"type\":\"selector\",\"value\":\"france\"},\"intervals\":{\"type\":\"intervals\",\"intervals\":[\"1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z\"]},\"limit\":10,\"order\":\"descending\",\"queryType\":\"scan\"}"))
-	spew.Dump(q, err)
-	d.Query().Execute(q, &results)
-	spew.Dump(results)
+	qry = `{"batchSize":20480,"columns":["__time","channel","cityName","comment","count","countryIsoCode","diffUrl","flags","isAnonymous","isMinor","isNew","isRobot","isUnpatrolled","metroCode","namespace","page","regionIsoCode","regionName","sum_added","sum_commentLength","sum_deleted","sum_delta","sum_deltaBucket","user"],"dataSource":{"type":"query","query":{"queryType":"scan","dataSource":{"type":"table","name":"A"},"columns":["AT"],"intervals":{"type":"intervals","intervals":["1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z"]}}},"filter":{"dimension":"countryName","extractionFn":{"locale":"","type":"lower"},"type":"selector","value":"france"},"intervals":{"type":"intervals","intervals":["1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z"]},"limit":10,"order":"descending","queryType":"scan"}`
+	//loadAndExecute(d, []byte(qry))
 
-	q, err = d.Query().Load([]byte("{\"context\":{\"con\":\"text\"},\"query\":\"SELECT \\\"__time\\\", \\\"channel\\\", \\\"cityName\\\", \\\"comment\\\", \\\"count\\\", \\\"countryIsoCode\\\", \\\"countryName\\\", \\\"diffUrl\\\", \\\"flags\\\", \\\"isAnonymous\\\", \\\"isMinor\\\", \\\"isNew\\\", \\\"isRobot\\\", \\\"isUnpatrolled\\\", \\\"metroCode\\\", \\\"namespace\\\", \\\"page\\\", \\\"regionIsoCode\\\", \\\"regionName\\\", \\\"sum_added\\\", \\\"sum_commentLength\\\", \\\"sum_deleted\\\", \\\"sum_delta\\\", \\\"sum_deltaBucket\\\", \\\"user\\\"\\nFROM \\\"wikipedia\\\"\\nWHERE \\\"countryName\\\" = 'France'\\nLIMIT 10\",\"queryType\":\"sql\", \"resultFormat\":\"array\", \"header\": true}"))
-	spew.Dump(q, err)
-	d.Query().Execute(q, &results)
-	spew.Dump(results)
+	qry = `{"context":{"con":"text"},"query":"SELECT \"__time\", \"channel\", \"cityName\", \"comment\", \"count\", \"countryIsoCode\", \"countryName\", \"diffUrl\", \"flags\", \"isAnonymous\", \"isMinor\", \"isNew\", \"isRobot\", \"isUnpatrolled\", \"metroCode\", \"namespace\", \"page\", \"regionIsoCode\", \"regionName\", \"sum_added\", \"sum_commentLength\", \"sum_deleted\", \"sum_delta\", \"sum_deltaBucket\", \"user\"\nFROM \"wikipedia\"\nWHERE \"countryName\" = 'France'\nLIMIT 10","queryType":"sql", "resultFormat":"array", "header": true}`
+	//loadAndExecute(d, []byte(qry))
 
-	q, err = d.Query().Load([]byte("{\"aggregations\":[{\"name\":\"count\",\"type\":\"count\"}],\"context\":{\"plop\":\"plep\"},\"dataSource\":{\"name\":\"wikipedia\",\"type\":\"table\"},\"filter\":{\"dimension\":\"countryName\",\"extractionFn\":null,\"type\":\"selector\",\"value\":\"France\"},\"granularity\":\"minute\",\"intervals\":{\"type\":\"intervals\",\"intervals\":[\"1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z\"]},\"limit\":10,\"postAggregations\":[],\"queryType\":\"timeseries\",\"virtualColumns\":[]}"))
-	spew.Dump(q, err)
-	d.Query().Execute(q, &results)
-	spew.Dump(results)
+	qry = `{"aggregations":[{"name":"count","type":"count"}],"context":{"plop":"plep"},"dataSource":{"name":"wikipedia","type":"table"},"filter":{"dimension":"countryName","extractionFn":null,"type":"selector","value":"France"},"granularity":"minute","intervals":{"type":"intervals","intervals":["1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z"]},"limit":10,"postAggregations":[],"queryType":"timeseries","virtualColumns":[]}`
+	loadAndExecute(d, []byte(qry))
 
-	q, err = d.Query().Load([]byte("{\"aggregations\":[{\"name\":\"count\",\"type\":\"count\"}],\"context\":{\"plop\":\"plep\"},\"dataSource\":{\"name\":\"wikipedia\",\"type\":\"table\"},\"filter\":{\"dimension\":\"countryName\",\"extractionFn\":null,\"type\":\"selector\",\"value\":\"France\"},\"granularity\":{\"duration\":1000,\"origin\":\"2016-06-27T13:30:00.000Z\",\"type\":\"duration\"},\"intervals\":{\"type\":\"intervals\",\"intervals\":[\"1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z\"]},\"limit\":10,\"postAggregations\":[],\"queryType\":\"timeseries\",\"virtualColumns\":[]}"))
-	spew.Dump(q, err)
-	d.Query().Execute(q, &results)
-	spew.Dump(results)
+	qry = `{"aggregations":[{"name":"count","type":"count"}],"context":{"plop":"plep"},"dataSource":{"name":"wikipedia","type":"table"},"filter":{"dimension":"countryName","extractionFn":null,"type":"selector","value":"France"},"granularity":{"duration":1000,"origin":"2016-06-27T13:30:00.000Z","type":"duration"},"intervals":{"type":"intervals","intervals":["1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z"]},"limit":10,"postAggregations":[],"queryType":"timeseries","virtualColumns":[]}`
+	loadAndExecute(d, []byte(qry))
 
-	q, err = d.Query().Load([]byte("{\"aggregations\":[{\"fieldName\":\"sum_delta\",\"name\":\"delta\",\"type\":\"longSum\"}],\"context\":{\"plop\":\"plep\"},\"dataSource\":{\"name\":\"wikipedia\",\"type\":\"table\"},\"dimension\":{\"dimension\":\"regionName\",\"outputName\":\"region\",\"outputType\":\"STRING\",\"type\":\"default\"},\"filter\":{\"dimension\":\"countryName\",\"extractionFn\":null,\"type\":\"selector\",\"value\":\"France\"},\"granularity\":\"hour\",\"intervals\":{\"type\":\"intervals\",\"intervals\":[\"1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z\"]},\"metric\":{\"metric\":\"delta\",\"type\":\"numeric\"},\"postAggregations\":[],\"queryType\":\"topN\",\"threshold\":50,\"virtualColumns\":[]}"))
-	spew.Dump(q, err)
-	d.Query().Execute(q, &results)
-	spew.Dump(results)
+	qry = `{"aggregations":[{"fieldName":"sum_delta","name":"delta","type":"longSum"}],"context":{"plop":"plep"},"dataSource":{"name":"wikipedia","type":"table"},"dimension":{"dimension":"regionName","outputName":"region","outputType":"STRING","type":"default"},"filter":{"dimension":"countryName","extractionFn":null,"type":"selector","value":"France"},"granularity":"hour","intervals":{"type":"intervals","intervals":["1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z"]},"metric":{"metric":"delta","type":"numeric"},"postAggregations":[],"queryType":"topN","threshold":50,"virtualColumns":[]}`
+	loadAndExecute(d, []byte(qry))
 
-	q, err = d.Query().Load([]byte("{\"aggregations\":[{\"fieldName\":\"sum_delta\",\"name\":\"delta\",\"type\":\"longSum\"}],\"context\":{\"plop\":\"plep\"},\"dataSource\":{\"name\":\"wikipedia\",\"type\":\"table\"},\"dimensions\":[{\"dimension\":\"countryName\",\"outputName\":\"country\",\"outputType\":\"STRING\",\"type\":\"default\"},{\"dimension\":\"regionName\",\"outputName\":\"region\",\"outputType\":\"STRING\",\"type\":\"default\"}],\"filter\":{\"dimension\":\"countryName\",\"extractionFn\":null,\"type\":\"selector\",\"value\":\"France\"},\"granularity\":\"minute\",\"intervals\":{\"type\":\"intervals\",\"intervals\":[\"1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z\"]},\"postAggregations\":[],\"queryType\":\"groupBy\",\"subtotalsSpec\":[],\"virtualColumns\":[]}"))
-	spew.Dump(q, err)
-	d.Query().Execute(q, &results)
-	spew.Dump(results)
+	qry = `{"aggregations":[{"fieldName":"sum_delta","name":"delta","type":"longSum"}],"context":{"plop":"plep"},"dataSource":{"name":"wikipedia","type":"table"},"dimensions":[{"dimension":"countryName","outputName":"country","outputType":"STRING","type":"default"},{"dimension":"regionName","outputName":"region","outputType":"STRING","type":"default"}],"filter":{"dimension":"countryName","extractionFn":null,"type":"selector","value":"France"},"granularity":"minute","intervals":{"type":"intervals","intervals":["1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z"]},"postAggregations":[],"queryType":"groupBy","subtotalsSpec":[],"virtualColumns":[]}`
+	loadAndExecute(d, []byte(qry))
 
-	q, err = d.Query().Load([]byte("{\"context\":{\"plop\":\"plep\"},\"dataSource\":{\"name\":\"wikipedia\",\"type\":\"table\"},\"filter\":{\"dimension\":\"countryName\",\"extractionFn\":null,\"type\":\"selector\",\"value\":\"France\"},\"intervals\":{\"type\":\"intervals\",\"intervals\":[\"1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z\"]},\"limit\":0,\"query\":{\"type\":\"contains\",\"value\":\"Allier\"},\"queryType\":\"search\",\"searchDimensions\":[{\"dimension\":\"regionName\",\"outputType\":\"STRING\",\"type\":\"default\"}]}"))
-	spew.Dump(q, err)
-	d.Query().Execute(q, &results)
-	spew.Dump(results)
+	qry = `{"context":{"plop":"plep"},"dataSource":{"name":"wikipedia","type":"table"},"filter":{"dimension":"countryName","extractionFn":null,"type":"selector","value":"France"},"intervals":{"type":"intervals","intervals":["1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z"]},"limit":0,"query":{"type":"contains","value":"Allier"},"queryType":"search","searchDimensions":[{"dimension":"regionName","outputType":"STRING","type":"default"}]}`
+	loadAndExecute(d, []byte(qry))
 
-	q, err = d.Query().Load([]byte("{\"bound\":\"minTime\",\"context\":{},\"dataSource\":{\"name\":\"wikipedia\",\"type\":\"table\"},\"intervals\":{\"type\":\"intervals\",\"intervals\":[\"1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z\"]},\"queryType\":\"timeBoundary\"}"))
-	spew.Dump(q, err)
-	d.Query().Execute(q, &results)
-	spew.Dump(results)
+	qry = `{"bound":"minTime","context":{},"dataSource":{"name":"wikipedia","type":"table"},"intervals":{"type":"intervals","intervals":["1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z"]},"queryType":"timeBoundary"}`
+	loadAndExecute(d, []byte(qry))
 
-	q, err = d.Query().Load([]byte("{\"context\":{\"a\":\"a\"},\"dataSource\":{\"name\":\"wikipedia\",\"type\":\"table\"},\"queryType\":\"dataSourceMetadata\"}"))
-	spew.Dump(q, err)
-	d.Query().Execute(q, &results)
-	spew.Dump(results)
+	qry = `{"context":{"a":"a"},"dataSource":{"name":"wikipedia","type":"table"},"queryType":"dataSourceMetadata"}`
+	//loadAndExecute(d, []byte(qry))
 
-	q, err = d.Query().Load([]byte("{\"analysisTypes\":[\"minmax\",\"interval\",\"size\",\"cardinality\",\"timestampSpec\",\"queryGranularity\",\"aggregators\",\"rollup\"],\"context\":{\"\":\"\"},\"dataSource\":{\"name\":\"wikipedia\",\"type\":\"table\"},\"intervals\":{\"type\":\"intervals\",\"intervals\":[\"1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z\"]},\"queryType\":\"segmentMetadata\"}"))
-	spew.Dump(q, err)
-	d.Query().Execute(q, &results)
-	spew.Dump(results)
+	qry = `{"analysisTypes":["minmax","interval","size","cardinality","timestampSpec","queryGranularity","aggregators","rollup"],"context":{"":""},"dataSource":{"name":"wikipedia","type":"table"},"intervals":{"type":"intervals","intervals":["1980-06-12T22:30:00.000Z/2020-01-26T23:00:00.000Z"]},"queryType":"segmentMetadata"}`
+	loadAndExecute(d, []byte(qry))
 }
