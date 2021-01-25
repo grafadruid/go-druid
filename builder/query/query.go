68b4756c9f198ebe0e280553fd2f4b4e6b9215a7
Vigith Maurice <vigith@gmail.com>
2021-01-25 07:47:09 -0800

    make intervals optional

--- builder/query/query.go
+++ builder/query/query.go
@@ -64,9 +64,14 @@ func (b *Base) UnmarshalJSON(data []byte) error {
 			d.(*datasource.Query).UnmarshalJSONWithQueryLoader(tmp.DataSource, Load)
 		}
 		b.DataSource = d
-		i, err := intervals.Load(tmp.Intervals)
-		if err != nil {
-			return err
+		// intervals are optional in some cases, eg dataSourceMetadata
+		// https://druid.apache.org/docs/latest/querying/datasourcemetadataquery.html
+		var i builder.Intervals
+		if tmp.Intervals != nil {
+			i, err = intervals.Load(tmp.Intervals)
+			if err != nil {
+				return err
+			}
 		}
 		b.Intervals = i
 	}
