ce18386712fc96e3e0767467db3b5d409e29ace7
Vigith Maurice <vigith@gmail.com>
2021-02-19 10:06:34 -0800

    godoc for scan (#30)
    * go mod tidy
    * first doc draft

--- builder/query/scan.go
+++ builder/query/scan.go
@@ -16,6 +16,8 @@ const (
 	None             = "NONE"
 )
 
+// Scan query returns raw Apache Druid rows in streaming mode.
+// https://druid.apache.org/docs/latest/querying/scan-query.html
 type Scan struct {
 	Base
 	VirtualColumns []builder.VirtualColumn `json:"virtualColumns,omitempty"`
@@ -29,72 +31,98 @@ type Scan struct {
 	Legacy         bool                    `json:"legacy,omitempty"`
 }
 
+// NewScan returns *builder.Scan which can be used to build a scan query.
+// Eg,
+//		table := datasource.NewTable().SetName("table-name")
+//
+//		now := time.Now()
+//		i := intervals.NewInterval().SetInterval(now.Add(-60*time.Minute), now)
+//		is := intervals.NewIntervals().SetIntervals([]*intervals.Interval{i})
+//
+//		filter1 := filter.NewSelector().SetDimension("key1").SetValue("val1")
+//		filter2 := filter.NewSelector().SetDimension("key2").SetValue("val2")
+//		filters := filter.NewAnd().SetFields([]builder.Filter{filter1, filter2})
+//
+//		ts := query.NewScan().SetDataSource(table).SetIntervals(is).SetFilter(filters).SetResultFormat("compactedList").SetLimit(10)
 func NewScan() *Scan {
 	s := &Scan{}
 	s.Base.SetQueryType("scan")
 	return s
 }
 
+// SetDataSource sets data source.
 func (s *Scan) SetDataSource(dataSource builder.DataSource) *Scan {
 	s.Base.SetDataSource(dataSource)
 	return s
 }
 
+// SetIntervals set the intervals.
 func (s *Scan) SetIntervals(intervals builder.Intervals) *Scan {
 	s.Base.SetIntervals(intervals)
 	return s
 }
 
+// SetContext sets the context.
 func (s *Scan) SetContext(context map[string]interface{}) *Scan {
 	s.Base.SetContext(context)
 	return s
 }
 
+// SetVirtualColumns sets virtual columns.
 func (s *Scan) SetVirtualColumns(virtualColumns []builder.VirtualColumn) *Scan {
 	s.VirtualColumns = virtualColumns
 	return s
 }
 
+// SetResultFormat sets the result format.
 func (s *Scan) SetResultFormat(resultFormat string) *Scan {
 	s.ResultFormat = resultFormat
 	return s
 }
 
+// SetBatchSize sets the batch size.
 func (s *Scan) SetBatchSize(batchSize int64) *Scan {
 	s.BatchSize = batchSize
 	return s
 }
 
+// SetLimit sets the limit.
 func (s *Scan) SetLimit(limit int64) *Scan {
 	s.Limit = limit
 	return s
 }
 
+// SetOffset sets the offset.
 func (s *Scan) SetOffset(offset int64) *Scan {
 	s.Offset = offset
 	return s
 }
 
+// SetOrder sets the order.
 func (s *Scan) SetOrder(order Order) *Scan {
 	s.Order = order
 	return s
 }
 
+// SetFilter sets the filter.
 func (s *Scan) SetFilter(filter builder.Filter) *Scan {
 	s.Filter = filter
 	return s
 }
 
+// SetColumns set columns.
 func (s *Scan) SetColumns(columns []string) *Scan {
 	s.Columns = columns
 	return s
 }
 
+// SetLegacy sets the `druid.query.scan.legacy` field.
 func (s *Scan) SetLegacy(legacy bool) *Scan {
 	s.Legacy = legacy
 	return s
 }
 
+// UnmarshalJSON unmarshalls a druid scan native query json string into builder type.
 func (s *Scan) UnmarshalJSON(data []byte) error {
 	var err error
 	var tmp struct {
