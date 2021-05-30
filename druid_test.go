152f016c12cfdbf09d193321cd35017e06d4ebb7
Marcelo Pereira <marcelovitor.pereira@gmail.com>
2021-05-30 12:23:12 +0200

    Override default request retry checker

--- druid_test.go
+++ druid_test.go
@@ -1,8 +1,12 @@
 package druid
 
 import (
+	"context"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -34,3 +38,100 @@ func TestNewClientWithSkipVerify(t *testing.T) {
 }
 
 // TODO: at some point use https://golang.org/src/crypto/tls/example_test.go this to create server with bad cert and test
+
+func TestDefaultRetry(t *testing.T) {
+	ctx := context.TODO()
+	var b string
+	resp := buildMockResp(200, b)
+	retry, err := defaultRetry(ctx, &resp, nil)
+	assert.Nil(t, err)
+	assert.False(t, retry)
+
+	b = `{
+		"error": "SQL parse failed", "errorMessage" : "Something bad happened."
+	}`
+	resp = buildMockResp(400, b)
+	retry, err = defaultRetry(ctx, &resp, nil)
+	assert.NotNil(t, err)
+	assert.False(t, retry)
+
+	b = `{
+		"error": "Plan validation failed", "errorMessage" : "Something bad happened."
+	}`
+	resp = buildMockResp(400, b)
+	retry, err = defaultRetry(ctx, &resp, nil)
+	assert.NotNil(t, err)
+	assert.False(t, retry)
+
+	b = `{
+		"error": "Resource limit exceeded", "errorMessage" : "Something bad happened."
+	}`
+	resp = buildMockResp(400, b)
+	retry, err = defaultRetry(ctx, &resp, nil)
+	assert.NotNil(t, err)
+	assert.True(t, retry)
+
+	b = `{
+		"error": "Query capacity exceeded", "errorMessage" : "Something bad happened."
+	}`
+	resp = buildMockResp(429, b)
+	retry, err = defaultRetry(ctx, &resp, nil)
+	assert.NotNil(t, err)
+	assert.True(t, retry)
+
+	b = `{
+		"error": "Unsupported operation", "errorMessage" : "Something bad happened."
+	}`
+	resp = buildMockResp(501, b)
+	retry, err = defaultRetry(ctx, &resp, nil)
+	assert.NotNil(t, err)
+	assert.False(t, retry)
+
+	b = `{
+		"error": "Query timeout", "errorMessage" : "Something bad happened."
+	}`
+	resp = buildMockResp(504, b)
+	retry, err = defaultRetry(ctx, &resp, nil)
+	assert.NotNil(t, err)
+	assert.True(t, retry)
+
+	b = `{
+		"error": "Query cancelled", "errorMessage" : "Something bad happened."
+	}`
+	resp = buildMockResp(500, b)
+	retry, err = defaultRetry(ctx, &resp, nil)
+	assert.NotNil(t, err)
+	assert.False(t, retry)
+
+	b = `{
+		"error": "Unknown exception", "errorMessage" : "Something bad happened."
+	}`
+	resp = buildMockResp(500, b)
+	retry, err = defaultRetry(ctx, &resp, nil)
+	assert.NotNil(t, err)
+	assert.False(t, retry)
+}
+
+func buildMockResp(statusCode int, body string) http.Response {
+	var st string
+	switch statusCode {
+	case 200:
+		st = "200 OK"
+	case 400:
+		st = "400 Bad Request"
+	case 429:
+		st = "429 Too Many Requests"
+	case 500:
+		st = "500 Internal Server Error"
+	case 501:
+		st = "Not Implemented"
+	case 504:
+		st = "Gateway Timeout"
+	default:
+		panic(fmt.Errorf("Unsupported mock status code: %d", statusCode))
+	}
+	return http.Response{
+		Status: st, StatusCode: statusCode,
+		Body: ioutil.NopCloser(strings.NewReader(body)),
+	}
+}
