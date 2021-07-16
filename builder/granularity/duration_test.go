704f9ee8fe0017ee9e2666b751bdb66bab12f13e
Jean-Baptiste Guerraz <861556+jbguerraz@users.noreply.github.com>
2021-07-16 09:22:19 +0000

    fix duration and period tests

--- builder/granularity/duration_test.go
+++ builder/granularity/duration_test.go
@@ -11,12 +11,11 @@ func TestNewDuration(t *testing.T) {
 	d := NewDuration()
 	d.SetDuration(time.Minute)
 	d.SetOrigin(time.Unix(1613779200, 0))
-	start, _ := time.Parse(time.RFC822, "19 Feb 21 19:00 EST")
 
 	x := &Duration{
 		Base:     Base{Typ: "duration"},
 		Duration: 60000000000,
-		Origin:   start,
+		Origin:   time.Unix(1613779200, 0),
 	}
 	assert.Equal(t, x, d)
 }
