f9a953743e4215e79aa97b9c18c72c0eb61bd712
shrivardhan <shrivardhan92@gmail.com>
2021-09-14 00:41:01 -0700

    Fix outputType struct tag for VirtualColumn expressions  (#51)
    * exclude examples from build
    
    * fix outputType on expressions
    
    Co-authored-by: Shrivardhan Rao <Shrivardhan_Rao@intuit.com>

--- builder/virtualcolumn/expression.go
+++ builder/virtualcolumn/expression.go
@@ -1,19 +1,12 @@
 package virtualcolumn
 
-type OutputType string
-
-const (
-	Long   OutputType = "LONG"
-	Float             = "FLOAT"
-	Double            = "DOUBLE"
-	String            = "STRING"
-)
+import "github.com/grafadruid/go-druid/builder/types"
 
 type Expression struct {
 	Base
-	Name       string `json:"name,omitempty"`
-	Expression string `json:"expression,omitempty"`
-	OutputType OutputType
+	Name       string           `json:"name,omitempty"`
+	Expression string           `json:"expression,omitempty"`
+	OutputType types.OutputType `json:"outputType,omitempty"`
 }
 
 func NewExpression() *Expression {
@@ -32,7 +25,7 @@ func (e *Expression) SetExpression(expression string) *Expression {
 	return e
 }
 
-func (e *Expression) SetOutputType(outputType OutputType) *Expression {
+func (e *Expression) SetOutputType(outputType types.OutputType) *Expression {
 	e.OutputType = outputType
 	return e
 }
