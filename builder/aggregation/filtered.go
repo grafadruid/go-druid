60cfad2f3f4e86d8c75e58322c62a58c60b3488d
shrivardhan <shrivardhan92@gmail.com>
2021-09-22 02:31:16 -0700

    Fix filtered aggregator (#54)
    * exclude examples from build
    
    * fix outputType on expressions
    
    * Fix filtered aggregator
    
    * clean up
    
    Co-authored-by: Shrivardhan Rao <Shrivardhan_Rao@intuit.com>

--- builder/aggregation/filtered.go
+++ builder/aggregation/filtered.go
@@ -1,9 +1,15 @@
 package aggregation
 
+import (
+	"encoding/json"
+	"github.com/grafadruid/go-druid/builder"
+	"github.com/grafadruid/go-druid/builder/filter"
+)
+
 type Filtered struct {
 	Base
-	Aggregator string
-	Filter     string
+	Aggregator builder.Aggregator `json:"aggregator,omitempty"`
+	Filter     builder.Filter     `json:"filter,omitempty"`
 }
 
 func NewFiltered() *Filtered {
@@ -15,4 +21,37 @@ func NewFiltered() *Filtered {
 func (f *Filtered) SetName(name string) *Filtered {
 	f.Base.SetName(name)
 	return f
+}
+
+func (f *Filtered) SetAggregator(aggregator builder.Aggregator) *Filtered {
+	f.Aggregator = aggregator
+	return f
+}
+
+func (f *Filtered) SetFilter(filter builder.Filter) *Filtered {
+	f.Filter = filter
+	return f
+}
+
+func (f *Filtered) UnmarshalJSON(data []byte) error {
+	var tmp struct {
+		Base
+		Aggregator json.RawMessage `json:"aggregator,omitempty"`
+		Filter     json.RawMessage `json:"filter,omitempty"`
+	}
+	if err := json.Unmarshal(data, &tmp); err != nil {
+		return err
+	}
+	a, err := Load(tmp.Aggregator)
+	if err != nil {
+		return err
+	}
+	filter, err := filter.Load(tmp.Filter)
+	if err != nil {
+		return err
+	}
+	f.Base = tmp.Base
+	f.Aggregator = a
+	f.Filter = filter
+	return nil
 }
