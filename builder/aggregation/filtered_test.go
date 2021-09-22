dd0e5f9d564c839d2dde8d72205a4d029a0631fb
Shrivardhan Rao <Shrivardhan_Rao@intuit.com>
2021-09-22 01:22:57 -0700

    Fix filtered aggregator

---
+++ builder/aggregation/filtered_test.go
@@ -0,0 +1,63 @@
+package aggregation
+
+import (
+	"encoding/json"
+	"github.com/grafadruid/go-druid/builder/filter"
+	"github.com/grafadruid/go-druid/builder/types"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestLoadFilteredAggregator(t *testing.T) {
+	assert := assert.New(t)
+
+	f, err := Load([]byte(`{
+			  "type": "filtered",
+			  "aggregator": {
+				"type": "count",
+				"name": "a1"
+			  },
+			  "filter": {
+				"type": "bound",
+				"dimension": "__time",
+				"lower": "1629136173000",
+				"upper": null,
+				"lowerStrict": true,
+				"upperStrict": false,
+				"extractionFn": null,
+				"ordering": "numeric"
+			  },
+			  "name": "a1"
+			}`))
+	//fmt.Printf("%+v\n%+v\n%+v\n", f, f.(*Filtered).Filter, f.(*Filtered).Aggregator)
+	assert.NotNil(f)
+	assert.Nil(err, "error should be nil")
+}
+
+func TestNewFiltered(t *testing.T) {
+	f := filter.NewBound().SetLower("1629136173000").SetLowerStrict(true).SetDimension("__time").SetOrdering(types.Numeric).
+		SetUpperStrict(false)
+	a := NewCount().SetName("a1")
+	fa := NewFiltered().SetFilter(f).SetAggregator(a).SetName("a1")
+
+	expected := `{
+      "type": "filtered",
+      "aggregator": {
+        "type": "count",
+        "name": "a1"
+      },
+      "filter": {
+        "type": "bound",
+        "dimension": "__time",
+        "lower": "1629136173000",
+        "lowerStrict": true,
+        "upperStrict": false,
+        "ordering": "numeric"
+      },
+      "name": "a1"
+    }`
+
+	faJSON, err := json.Marshal(fa)
+	assert.Nil(t, err)
+	assert.JSONEq(t, expected, string(faJSON))
+}
