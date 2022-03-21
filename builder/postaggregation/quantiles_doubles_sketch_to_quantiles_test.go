b94021e455698a62fa64812b6a2ba56a4805e05b
Chrome <19543684+chromevoid@users.noreply.github.com>
2022-03-21 09:51:53 -0700

    Tdigest post aggregator (#64)
    * add tdigest post agg
    
    * update example
    
    * update comment
    
    * update test
    
    * fix comment
    
    * update tests
    
    Co-authored-by: jyu6 <juanlu_yu@intuit.com>

--- builder/postaggregation/quantiles_doubles_sketch_to_quantiles_test.go
+++ builder/postaggregation/quantiles_doubles_sketch_to_quantiles_test.go
@@ -8,25 +8,25 @@ import (
 
 func TestQuantilesDoublesSketchToQuantiles(t *testing.T) {
 	qf := NewQuantilesDoublesSketchField()
-	qf.SetType("fieldAccess").SetName("tp90").SetFieldName("a1:agg")
+	qf.SetType("fieldAccess").SetName("tp75tp90").SetFieldName("a1:agg")
 	quantilesDoublesSketchToQuantiles := NewQuantilesDoublesSketchToQuantiles()
-	quantilesDoublesSketchToQuantiles.SetName("tp90").SetField(qf).SetFractions([]float64{0.90})
+	quantilesDoublesSketchToQuantiles.SetName("tp75tp90").SetField(qf).SetFractions([]float64{0.75, 0.90})
 
 	// "omitempty" will ignore boolean=false
 	expected := `
 {
   "type": "quantilesDoublesSketchToQuantiles",
-  "name": "tp90",
+  "name": "tp75tp90",
   "field": {
     "type": "fieldAccess",
-    "name": "tp90",
+    "name": "tp75tp90",
     "fieldName": "a1:agg"
   },
-  "fractions": [0.9]
+  "fractions": [0.75, 0.9]
 }
 `
 
-	quantilesDoublesSketchToQuantileJSON, err := json.Marshal(quantilesDoublesSketchToQuantiles)
+	quantilesDoublesSketchToQuantilesJSON, err := json.Marshal(quantilesDoublesSketchToQuantiles)
 	assert.Nil(t, err)
-	assert.JSONEq(t, expected, string(quantilesDoublesSketchToQuantileJSON))
+	assert.JSONEq(t, expected, string(quantilesDoublesSketchToQuantilesJSON))
 }
