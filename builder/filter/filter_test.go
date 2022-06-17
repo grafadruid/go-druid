f408f3cd7569e316ad0529d67cc88cb5bef9924d
Shrivardhan Rao <Shrivardhan_Rao@intuit.com>
2022-06-16 21:25:03 -0700

    fix filter intervals

--- builder/filter/filter_test.go
+++ builder/filter/filter_test.go
@@ -1,9 +1,13 @@
 package filter
 
 import (
-	"testing"
-
+	"encoding/json"
+	"fmt"
+	"github.com/grafadruid/go-druid/builder"
+	"github.com/grafadruid/go-druid/builder/intervals"
 	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
 )
 
 func TestLoadUnsupportedType(t *testing.T) {
@@ -11,7 +15,38 @@ func TestLoadUnsupportedType(t *testing.T) {
 
 	f, err := Load([]byte("{\"type\": \"blahblahType\"}"))
 
-	assert.Nil(f, "filter should be nil")
-	assert.NotNil(err, "error should not be nil")
-	assert.Error(err, "unsupported filter type")
+	assert.Nil(f,
+		"filter should be nil")
+	assert.NotNil(err,
+		"error should not be nil")
+	assert.Error(err,
+		"unsupported filter type")
+}
+
+func TestNewInterval(t *testing.T) {
+	location, _ := time.LoadLocation("UTC")
+	start, _ := time.ParseInLocation(time.RFC3339Nano,
+		"2022-06-16T08:28:53.33441Z",
+		location)
+	end, _ := time.ParseInLocation(time.RFC3339Nano,
+		"2022-06-16T15:28:53.33441Z",
+		location)
+	// simple interval
+	i := intervals.NewInterval().SetInterval(start,
+		end)
+	filter1 := NewSelector().SetDimension("countryName").SetValue("France")
+	filterInterval := NewInterval().SetIntervals([]*intervals.Interval{i}).SetDimension("__time")
+	filters := NewOr().SetFields([]builder.Filter{filter1, filterInterval})
+
+	f, err := json.Marshal(filters)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	assert.Nil(t,
+		err)
+	assert.Equal(t,
+		`{"type":"or","fields":[{"type":"selector","dimension":"countryName","value":"France"},{"type":"interval","dimension":"__time","intervals":["2022-06-16T08:28:53.33441Z/2022-06-16T15:28:53.33441Z"]}]}`,
+		string(f),
+		"filter with time interval")
 }
