3dc969f356e6af5021dc7376a321d02f6e801269
Allen Kim <pig3653@naver.com>
2023-02-08 02:18:45 +0900

    Added sql test code and modified some code of sql.go (#80)
    * Change the value type of SQLParameter and modify the constructor, Add SetContext method
    
    * Add sql test code
    
    * A bit more concise example code.
    
    ---------
    
    Co-authored-by: allen.k1m <allen.k1m@kakaocorp.com>

---
+++ examples/querySqlExample.go
@@ -0,0 +1,35 @@
+//go:build mage
+// +build mage
+
+package main
+
+import (
+	"github.com/davecgh/go-spew/spew"
+	"github.com/grafadruid/go-druid"
+	"github.com/grafadruid/go-druid/builder/query"
+	"log"
+)
+
+func main() {
+	d, err := druid.NewClient("http://localhost:8888")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var results []map[string]interface{}
+	var param []query.SQLParameter
+	param = append(param, query.NewSQLParameter("VARCHAR", "Salo Toraut"))
+	param = append(param, query.NewSQLParameter("VARCHAR", "NB"))
+	param = append(param, query.NewSQLParameter("VARCHAR", "false")) // BOOLEAN type fails the convent in api.
+	param = append(param, query.NewSQLParameter("INTEGER", 31))
+	context := make(map[string]interface{})
+	context["sqlTimeZone"] = "America/Los_Angeles"
+	query := query.NewSQL().SetQuery(
+		`SELECT * FROM "wikipedia" WHERE page=? AND flags=? AND isUnpatrolled=? AND delta=?`).
+		SetParameters(param).
+		SetContext(context)
+	_, err = d.Query().Execute(query, &results)
+	if err != nil {
+		log.Fatal(err)
+	}
+	spew.Dump(results)
+}
