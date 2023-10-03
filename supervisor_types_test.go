b1ed02589261f37eda71ce74ffc0b0d8ec50ecae
Victor Zaytsev <94850767+vzaytsev1981@users.noreply.github.com>
2023-10-03 11:12:20 -0400

    update dimension spec fields and add ingestion spec options helpers (#6)
    * update dimension spec fields and add ingestion spec options helpers

--- supervisor_types_test.go
+++ supervisor_types_test.go
@@ -85,7 +85,6 @@ var jsonBasic = `{
         }
     },
     "ioConfig": {
-        "type": "kafka",
         "topic": "test_topic",
         "consumerProperties": {
             "bootstrap.servers": "test_brokers"
